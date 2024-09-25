b2c3521a21e6db46c5e0a15001fb2852a21fe99a
Asif Akbar <786asifakbar53@gmail.com>
2024-09-25 23:33:41 +0500

    remove hero section

--- help/src/pages/Home.jsx
+++ help/src/pages/Home.jsx
@@ -1,13 +1,24 @@
-import HeroSection from "./HeroSection";
-
+// src/components/Home.js
+import SuportPage from './SuportPage';
 
 const Home = () => {
   return (
     <>
-      <HeroSection/>
-
+    <div className="relative w-screen h-screen overflow-hidden">
+      {/* Background Video */}
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute top-0 left-0 w-auto h-screen lg:w-screen lg:h-full md:w-full md:h-screen sm:w-full sm:h-screen  object-cover"
+      >
+        <source src="/video.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+    <SuportPage/>
     </>
   );
-}
+};
 
-export default Home
+export default Home;
