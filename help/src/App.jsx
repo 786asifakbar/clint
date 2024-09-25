ad3271b78a82da3e130c93f3ed7257e907565d34
Asif Akbar <786asifakbar53@gmail.com>
2024-09-26 00:21:13 +0500

    buildfile

--- help/src/App.jsx
+++ help/src/App.jsx
@@ -1,10 +1,10 @@
-import {BrowserRouter , Route, Routes } from 'react-router-dom';
+import {BrowserRouter , Routes , Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import OurMember from './pages/OurMember';
 import Contact from "./pages/Contact";
-import ShadiMubark from './pages/ShadiMubark'
+import ShadiMubark from './pages/ShadiMubark';
 import FoodAid from './pages/FoodAid';
 import HelpEducation from './pages/HelpEducation';
 import Health from './pages/Health';
